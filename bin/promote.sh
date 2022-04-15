b3d2614ebb70665ec180c63a806bc6bee9232f24
dorseytc <tpwgp38@yahoo.com>
2022-04-15 13:00:50 -0400

    Add wildcard support

--- bin/promote.sh
+++ bin/promote.sh
@@ -3,16 +3,16 @@
 #
 #  TDORSEY 2022-02-03  Created
 #
-if [ $# -eq 0 ]; then
-  echo "No arguments provided"
-else
-  echo Promoting $1
-  # assert sudo privs
-  sudo -v
-  if [ -f "$1" ]; then
-    if [ -f "/usr/local/bin/$1" ]; then
-      echo Target file /usr/local/bin/$1 exists
-      count=`diff -q $1 /usr/local/bin/$1 | wc -l`
+filecount=0
+promotecount=0
+for var in "$@"
+do 
+  echo Promoting $var
+  filecount=$((filecount+1))
+  if [ -f "$var" ]; then
+    if [ -f "/usr/local/bin/$var" ]; then
+      echo Target file /usr/local/bin/$var exists
+      count=`diff -q $var /usr/local/bin/$var | wc -l`
       if [ $count = 1 ]; then
         promote=Y
         echo Files are different
@@ -20,25 +20,27 @@ else
           mkdir .backup
           echo created directory .backup
         fi  
-        sudo mv /usr/local/bin/$1 .backup/$1
-        echo backed up $1 to .backup/$1
-        diff $1 .backup/$1
+        sudo mv /usr/local/bin/$var .backup/$var
+        echo backed up $var to .backup/$var
+        diff $var .backup/$var > .backup/$var.diff
       else
         promote=N
         echo Files are identical
-        echo Did not promote local file $1
+        echo Did not promote local file $var
       fi
     else
         promote=Y
     fi
     if [ "$promote" = "Y" ]; then
-      sudo cp $1 /usr/local/bin
-      sudo chown root /usr/local/bin/$1
-      sudo chgrp root /usr/local/bin/$1
-      sudo chmod 755 /usr/local/bin/$1
-      echo $1 promoted to /usr/local/bin
+      sudo cp $var /usr/local/bin
+      sudo chown root /usr/local/bin/$var
+      sudo chgrp root /usr/local/bin/$var
+      sudo chmod 755 /usr/local/bin/$var
+      echo $var promoted to /usr/local/bin
+      promotecount=$((promotecount+1))
     fi
   else
-    echo File "$1" not found
+    echo File "$var" not found
   fi
-fi
+done
+echo Promoted $promotecount of $filecount files
