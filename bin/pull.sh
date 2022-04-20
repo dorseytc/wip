2d9f3c260ee4381d94718f9146da678713be706f
dorseytc <tpwgp38@yahoo.com>
2022-04-20 19:52:45 -0400

    additional git helpers

--- bin/pull.sh
+++ bin/pull.sh
@@ -9,19 +9,15 @@
 
 source_dir=$1
 search_str=$2
-target_dir=$3
 datetime=`date +%Y%m%d_%H%M%S`
 filecount=0
 pullcount=0
 echo Running at ${datetime}
-if [ -z "${source_dir}" ] || [ -z "${search_str}" ] || [ -z "${target_dir}" ]; then
-  echo "usage: $0 source_dir search_str target_dir"
+if [ -z "${source_dir}" ] || [ -z "${search_str}" ]; then
+  echo "usage: $0 source_dir search_str"
 elif [ ! -d "${source_dir}" ]; then
   echo "Source directory $source_dir not found"
-elif [ ! -d "${target_dir}" ]; then
-  echo "Target directory $target_dir not found"
 else
-  cd $target_dir
   diff_ct=`git diff --stat origin/main | wc -l`
   echo "Diff count is $diff_ct"
   if [[ $diff_ct -eq 0  ]]; then
@@ -32,19 +28,12 @@ else
     cat /tmp/pull.list | while read f; do
       filecount=$((filecount+1))
       echo File is $f
-      target_file="${target_dir}${f}"
+      target_file="./$(basename $f)" 
       echo Target file is $target_file
       if [ -f "${target_file}" ]; then
         diffcount=`diff -q $f $target_file | wc -l`
         if [ $diffcount -eq 1 ]; then
           echo Files are different
-	        if [ ! -d "${target_dir}/.backup" ]; then
-            mkdir ${target_dir}/.backup 
-            echo created directory ${target_dir}/.backup
-	        fi
-           
-          mv $target_file ${target_dir}/.backup${f}.${datetime}
-	        echo backed up ${f} to ${target_dir}/.backup${f}.${datetime}
 	        pull=Y
         else
           echo Files are identical
---
+++ bin/reset.sh
@@ -0,0 +1,12 @@
+# pull updated scripts from other sources into working repository
+#git checkout -- filename
+
+#Assuming you added it to the index, but did not commit it, then:
+
+#git reset HEAD filename
+#git checkout -- filename
+
+#Assuming you did commit it, then:
+
+#git checkout origin/master filename
+
