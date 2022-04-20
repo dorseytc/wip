531fffaa3b9bfde7360d884e944e1dc27dcb6404
dorseytc <tpwgp38@yahoo.com>
2022-04-19 21:36:20 -0400

    fixing pull.sh

--- bin/pull.sh
+++ bin/pull.sh
@@ -15,11 +15,11 @@ filecount=0
 pullcount=0
 echo Running at ${datetime}
 if [ -z "${source_dir}" ] || [ -z "${search_str}" ] || [ -z "${target_dir}" ]; then
-   echo "usage: $0 source_dir search_str target_dir"
+  echo "usage: $0 source_dir search_str target_dir"
 elif [ ! -d "${source_dir}" ]; then
-   echo "Source directory $source_dir not found"
+  echo "Source directory $source_dir not found"
 elif [ ! -d "${target_dir}" ]; then
-   echo "Target directory $target_dir not found"
+  echo "Target directory $target_dir not found"
 else
   cd $target_dir
   diff_ct=`git diff --stat origin/main | wc -l`
@@ -29,28 +29,34 @@ else
     echo Files to be reviewed:
     cat /tmp/pull.list
     echo End of list
-    cat /tmp/pull.list | while read fqfile; do
-      echo FQFile is $fqfile
+    cat /tmp/pull.list | while read f; do
       filecount=$((filecount+1))
-      filename=$( basename "$fqfile" )
-      echo Filename is $filename
-      diffcount=`diff -q $fqfile ${target_dir}/${filename} | wc -l`
-      if [ $diffcount -eq 1 ]; then
-         echo Files are different
-	 if [ ! -d "${target_dir}/.backup" ]; then
-           mkdir ${target_dir}/.backup 
-	   echo created directory ${target_dir}/.backup
-	 fi
-         #mv ${target_dir}${filename} ${target_dir}/.backup$/{filename}.${datetime}
-	 echo backed up ${filename} to ${target_dir}/.backup/${filename}.${datetime}
-	 pull=Y
+      echo File is $f
+      target_file="${target_dir}${f}"
+      echo Target file is $target_file
+      if [ -f "${target_file}" ]; then
+        diffcount=`diff -q $f $target_file | wc -l`
+        if [ $diffcount -eq 1 ]; then
+          echo Files are different
+	        if [ ! -d "${target_dir}/.backup" ]; then
+            mkdir ${target_dir}/.backup 
+            echo created directory ${target_dir}/.backup
+	        fi
+           
+          mv $target_file ${target_dir}/.backup${f}.${datetime}
+	        echo backed up ${f} to ${target_dir}/.backup${f}.${datetime}
+	        pull=Y
+        else
+          echo Files are identical
+	        pull=N
+        fi
       else
-         echo Files are identical
-	 pull=N
-      fi
+        echo File "${target_file}" not found
+        pull=Y
+      fi 
       if [ "$pull" = "Y" ]; then
-        #cp $fqfile ${target_dir}/${filename}
-        echo $fqfile pulled to ${target_dir}/${filename}
+        cp $f ${target_file}
+        echo $f pulled to ${target_file}
         pullcount=$((pullcount+1))
       fi
     done; 
